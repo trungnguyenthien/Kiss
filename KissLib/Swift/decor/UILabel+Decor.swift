f9075b0e36d4ee7dc3ad9feb71bd7167889cfd33
Trung Nguyen Thien <ngthientrung@gmail.com>
2020-07-20 23:52:04 +0700

    no message

--- KissLib/Swift/decor/UILabel+Decor.swift
+++ KissLib/Swift/decor/UILabel+Decor.swift
@@ -10,35 +10,37 @@ import Foundation
 import UIKit
 
 public protocol TextDecorator {
-    func text(_ string: String) -> Self
-    func textColor(_ color: UIColor) -> Self
-    func fontSize(_ size: CGFloat) -> Self
-    func fontName(_ name: String) -> Self
+    @discardableResult func text(_ string: String) -> Self
+    @discardableResult func textColor(_ color: UIColor) -> Self
+    @discardableResult func fontSize(_ size: CGFloat) -> Self
+    @discardableResult func fontName(_ name: String) -> Self
 }
 
-extension TextDecorator where Self: UILabel {
-    public func text(_ string: String) -> Self {
+public extension TextDecorator where Self: UILabel {
+    @discardableResult func text(_ string: String) -> Self {
         text = string
         return self
     }
 
-    public func textColor(_ color: UIColor) -> Self {
+    @discardableResult func textColor(_ color: UIColor) -> Self {
         textColor = color
         return self
     }
 
-    public func fontSize(_ size: CGFloat) -> Self {
+    @discardableResult func fontSize(_ size: CGFloat) -> Self {
         guard let current = font else { return self }
         font = current.withSize(size)
         return self
     }
 
-    public func fontName(_ name: String) -> Self {
+    @discardableResult func fontName(_ name: String) -> Self {
         guard let size = font?.pointSize else { return self }
         font = UIFont(name: name, size: size)
         return self
     }
 }
+
+extension UILabel: TextDecorator {}
 
 public extension String {
     func label() -> UILabel {
--- KissLib/Swift/decor/UIView+Decor.swift
+++ KissLib/Swift/decor/UIView+Decor.swift
@@ -10,40 +10,37 @@ import Foundation
 import UIKit
 
 public protocol ViewDecorator {
-    @discardableResult
-    func background(color: UIColor) -> Self
-    @discardableResult
-    func stroke(size: Double) -> Self
-    @discardableResult
-    func stroke(color: UIColor) -> Self
-    @discardableResult
-    func cornerRadius(_ radius: Double) -> Self
-    @discardableResult
-    func corners(_ corners: UIRectCorner, radius: Double) -> Self
+    @discardableResult func background(color: UIColor) -> Self
+    @discardableResult func stroke(size: Double) -> Self
+    @discardableResult func stroke(color: UIColor) -> Self
+    @discardableResult func cornerRadius(_ radius: Double) -> Self
+    @discardableResult func corners(_ corners: UIRectCorner, radius: Double) -> Self
 }
 
+extension UIView: ViewDecorator {}
+
 public extension ViewDecorator where Self: UIView {
-    func background(color: UIColor) -> Self {
+    @discardableResult func background(color: UIColor) -> Self {
         backgroundColor = color
         return self
     }
 
-    func stroke(size: Double) -> Self {
+    @discardableResult func stroke(size: Double) -> Self {
         layer.borderWidth = CGFloat(size)
         return self
     }
 
-    func stroke(color: UIColor) -> Self {
+    @discardableResult func stroke(color: UIColor) -> Self {
         layer.borderColor = color.cgColor
         return self
     }
 
-    func cornerRadius(_ radius: Double) -> Self {
+    @discardableResult func cornerRadius(_ radius: Double) -> Self {
         corners(.allCorners, radius: radius)
         return self
     }
 
-    func corners(_ corners: UIRectCorner, radius: Double) -> Self {
+    @discardableResult func corners(_ corners: UIRectCorner, radius: Double) -> Self {
         // https://www.swiftdevcenter.com/uiview-round-specific-corners-only-swift/
         let size = CGSize(width: radius, height: radius)
         let bezierPath = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: size)
